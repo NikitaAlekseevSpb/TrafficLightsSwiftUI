070c35ff8350f7a3acf442162022769497742ef1
Nik <nik.alekseev.spb@gmail.com>
2021-06-01 19:21:35 +0300

    Initial Commit

---
+++ TrafficLightsUI/TrafficLightsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsUIApp.swift
+//  TrafficLightsUI
+//
+//  Created by MacBook on 01.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
